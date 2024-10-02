1deb87bba910e50cb5375b3cf5a511d1df4ab32c
rxy4728 <flynnu4728@outlook.com>
2024-10-02 09:52:40 +0800

    Attn Matmul Speed up 30%

--- tools/python/validate.py
+++ tools/python/validate.py
@@ -408,19 +408,22 @@ def validate_onnx_model(platform, model_file,
         mace_out_value = load_data(output_file_name)
         real_output_shape = output_shapes[i]
         real_output_data_format = DataFormat.NONE
-        # mace_out_value, real_output_shape, real_output_data_format = \
-        #     get_real_out_value_shape_df(platform,
-        #                                 mace_out_value,
-        #                                 output_shapes[i],
-        #                                 output_data_formats[i])
-        # print(f'tensor name: {output_names[i]}, shapes: {output_shapes[i]}, data_formats: {output_data_formats[i]}')
+        mace_out_value, real_output_shape, real_output_data_format = \
+            get_real_out_value_shape_df(platform,
+                                        mace_out_value,
+                                        output_shapes[i],
+                                        output_data_formats[i])
+        print(f'tensor name: {output_names[i]}, shapes: {output_shapes[i]}, data_formats: {output_data_formats[i]}')
         ##下面是用来生成一个tensor分别在mace和nn库上跑完的具体数值文件
-        tensor_names = ['46','61','75']
+        tensor_names = ['844']
         if output_names[i] in tensor_names:
-            mace_output_file = "/home/NIO/mace/workspace/attention/tensor_data/mace_" + output_names[i]
-            onnx_output_file = "/home/NIO/mace/workspace/attention/tensor_data/onnxruntime_" + output_names[i]
-            np.savetxt(mace_output_file, mace_out_value, '%.6f')
-            np.savetxt(onnx_output_file, value, '%.6f')
+            mace_output_file = "/home/ana/hhj/mace/workspace/attention/data/mace_" + output_names[i] + ".raw"
+            onnx_output_file = "/home/ana/hhj/mace/workspace/attention/data/onnxruntime_" + output_names[i] + ".raw"
+            # np.savetxt(mace_output_file, mace_out_value, '%.6f')
+            # np.savetxt(onnx_output_file, value, '%.6f')
+            mace_out_value.astype('float32').tofile(mace_output_file)
+            value.astype('float32').tofile(onnx_output_file)
+            print(f'value size: {value.size}')
         compare_output(output_names[i],
                        mace_out_value, value,
                        validation_threshold, log_file,
